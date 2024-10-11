2754d6a5266a5c2baeff8a784644c30f93f91f5a
M-H86 <101807059+M-H86@users.noreply.github.com>
2024-10-11 13:49:04 +0200

    BaseCommitV1

---
+++ CoolScripts/Sherlock_wkhtmltoimage_CombinedSearchandScrape.ps1
@@ -0,0 +1,81 @@
+<#
+REQUIREMENTS/Utilizes:
+
+https://wkhtmltopdf.org/docs.html
+https://github.com/sherlock-project/sherlock
+
+#>
+
+$outputFolder = ".\sherlockoutput\"
+$date = Get-Date -format dd/MM/yy_HH:mm:ss
+$logfile = ".\logs.txt"
+$ScraperExecutable = "PATH TO *\ wkhtmltoimage.exe"
+$sherlockEXE = "PATH TO  *\ Python\Python312\Scripts\Sherlock.exe"
+ 
+
+ #URL Scrape Component
+try {
+    # Read the list of names from the file
+    $names = Get-Content ".\input-namesList.txt"
+    
+    # Iterate over each name
+    foreach ($name in $names) {
+        $arg1 = $name
+        $arg2 = "-fo"
+        $arg3 = $outputFolder
+        
+        # Combine the arguments into a single string
+        $arguments = "$arg1 $arg2 $arg3"
+        
+        # Use Start-Process to run Sherlock.exe with arguments
+        Start-Process -FilePath $sherlockEXE -ArgumentList $arguments -NoNewWindow -Wait
+    }   
+}
+catch {
+    # Log any errors
+    "$date : $_" | Out-File $logfile -Append
+}
+
+#Screenshot Component
+try{
+# Define the path to the folder containing the text files
+$folderPath = ".\sherlockoutput\"
+
+# Get all text files from the folder
+$textFiles = Get-ChildItem -Path $folderPath -Filter *.txt
+
+# Loop through each text file
+foreach ($file in $textFiles) {
+    # Get the file name without extension to use as the folder name
+    $fileName = [System.IO.Path]::GetFileNameWithoutExtension($file.FullName)
+    
+    # Create a folder for the output using the file name
+    $outputFolder = "$folderPath\$fileName"
+    if (-not (Test-Path -Path $outputFolder)) {
+        New-Item -ItemType Directory -Path $outputFolder
+    }
+
+    # Read the content of the text file
+    $lines = Get-Content -Path $file.FullName
+    
+    # Process each line (you can customize this section as per your needs)
+    foreach ($line in $lines) {
+        # Example action: write each line to a separate file within the folder
+        #$outputFile = "$outputFolder\Output_$(Get-Random).txt"
+        #Set-Content -Path $outputFile -Value $line
+        $args1 = "$line"
+        $arguments2 = "$args1 $outputFolder"
+
+  Start-Process $ScraperExecutable -ArgumentList $arguments2 -NoNewWindow -Wait
+  
+     }
+}
+
+Write-Host "Processing complete!"
+
+}
+catch
+{
+    "$date : $_" | Out-File $logfile -Append
+}
+
